bf57c553d44179a761800c503f7f47c897924f40
Sofia Bejerano <44761938+sofiadanielle@users.noreply.github.com>
2020-04-03 19:06:43 -0700

    updated main

--- main.cpp
+++ main.cpp
@@ -6,18 +6,18 @@ int main(int argc, char **argv){
   bool run = true; // repeat
 
   Delimeter *item = new Delimeter();
-  GenStack<string> *mainStack = d->stack;
+//  GenStack<string> *mainStack = d->stack;
 
   cout << argv[2] << endl;
-  if(d->Stack(argv[2]) == false){ // argument isn't met
+  if(item->stack(argv[2]) == false){ // argument isn't met
     return -1;
   }
 
   for(int i = item->lineCount; i > 0; --i){ // for loop
-    if(!item->readString(mainStack->pop(), i)){
+    if(!item->readString(stack->pop(), i)){
       run = false;
       break;
     }
   }
-  
+
 }
